3450f394b8de6a7800298005cd1d50935faa4714
Matt Woelk <matt@mattwoelk.ca>
2014-02-07 15:04:33 -0600

    Proper loading of marks

--- autoload/test.py
+++ autoload/test.py
@@ -1,12 +1,22 @@
 import vim
 
 
-possible_marks = ['abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'] # TODO add more options here TODO make customizable
+# TODO add more options here TODO make customizable
+possible_marks = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 def drr():
-    marks = {}
-    marks['a'] = vim.current.buffer.mark('a')
+    marks = get_mark_locations()
     #marks = vim.command("marks").split()
     print(marks['a'])
     print(";)")
+
+
+def get_mark_locations():
+    result = {}
+    for mark in possible_marks:
+        mark_location = vim.current.buffer.mark(mark)
+        if mark_location:
+            mark_location = {'row': mark_location[0], 'col': mark_location[1]}
+        result[mark] = mark_location
+    return result
